1fa8091f338e63c0e3c3f4118ba574f0fce9a1c6
KryzPetr1533 <petrassassin03@gmail.com>
2023-01-08 23:47:00 +0300

    Adding ast

--- compare.py
+++ compare.py
@@ -1,5 +1,6 @@
 import argparse
 import numpy as np
+import ast
 
 parser = argparse.ArgumentParser()
 parser.add_argument('input_file', help='path to the input file .txt that contains paths to code files')
@@ -9,7 +10,7 @@ in_file = args.input_file  # Name of the file with input.
 out_file = args.output_file  # Name of the file for the answer.
 
 
-def levenstein(str1, str2):  # returns number -- levenstein distance, how different lines are.
+def levenstein(str1, str2):  # Returns number -- levenstein distance, how different lines are.
     matrix = [[i + j if i * j == 0 else 0 for j in range(len(str2) + 1)] for i in range(len(str1) + 1)]
     for i in range(1, len(str1) + 1):
         for j in range(1, len(str2) + 1):
@@ -24,17 +25,43 @@ answers = []
 with open(in_file, mode='r', encoding='utf-8') as code_files:
     files_paths = code_files.read().split()  # Array of names of files with code.
     for index in range(0, len(files_paths) - 1, 2):
-        with open(files_paths[index], mode='r', encoding='utf-8') as code1, open(files_paths[index + 1], mode='r',
-                                                                                 encoding='utf-8') as code2:
-            lines1 = code1.read().replace(' ', '').split()  # getting rid of everything but words
-            lines2 = code2.read().replace(' ', '').split()
-            ans = 0
-            symbols = 0
-            for line1, line2 in zip(lines1, lines2):
-                ans += levenstein(line1, line2)
-                symbols += np.max([len(line1), len(line2)])
+        with open(files_paths[index], mode='r', encoding='utf-8') as file1, open(files_paths[index + 1], mode='r',
+                                                                                 encoding='utf-8') as file2:
+            code1 = file1.read()
+            code2 = file2.read()
+
+        tree1 = ast.parse(code1)
+        tree2 = ast.parse(code2)
+
+
+        def docstr_remover(tree):  # Removes docstrings.
+            for node in ast.walk(tree):
+                if not isinstance(node, (ast.FunctionDef, ast.ClassDef, ast.AsyncFunctionDef)):
+                    continue
+                if not len(node.body):
+                    continue
+                if not isinstance(node.body[0], ast.Expr):
+                    continue
+                if not hasattr(node.body[0], 'value') or not isinstance(node.body[0].value, ast.Str):
+                    continue
+                node.body = node.body[1:]
+
+
+        docstr_remover(tree1)
+        docstr_remover(tree2)
+
+        lines1 = (ast.dump(tree1, annotate_fields=False)).split()  # Split into lines, so it takes less memory.
+        lines2 = (ast.dump(tree2, annotate_fields=False)).split()
+        ans = 0
+        symbols = 0
+        for line1, line2 in zip(lines1, lines2):
+            ans += levenstein(line1, line2)
+            symbols += np.max([len(line1), len(line2)])
+        if ans * symbols == 0:
+            answers.append(0.0)
+        else:
             answers.append(ans / symbols)
 
-with open(out_file, mode='w', encoding='utf-8') as output:  # write into file
+with open(out_file, mode='w', encoding='utf-8') as output:  # Write into file.
     for res in answers:
-        output.write('{:.3}\n'.format(res))
+        output.write('{:.3f}\n'.format(res))
